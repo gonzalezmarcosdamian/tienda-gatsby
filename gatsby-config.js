2ddefe042f250e1724c0f85059575b3e6dae78b9
Marcos damian Gonzalez <ingonzalezdamian@gmail.com>
2020-06-18 17:29:43 -0300

    Stripe error

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,10 +3,20 @@
  *
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
+require("dotenv").config({ path: `.env.${process.env.NODE_ENV}` })
 
 module.exports = {
   /* Your site config here */
-  plugins: [`gatsby-plugin-postcss`, `gatsby-plugin-react-helmet`],
+  plugins: [
+    `gatsby-plugin-postcss`,
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-stripe`,
+      objects: ["Product", "Price", "Sku"],
+      secretKey: process.env.STRIPE_SECRET_KEY,
+      downloadFiles: true,
+    },
+  ],
   siteMetadata: {
     title: "CF tienda",
     url: "https://codigofacilito.com",
